ad5b1d6a97821b4265dbe4548146e634a7282f49
PrashamW <cs21btech11047@iith.ac.in>
2022-07-10 02:48:06 +0530

    Upload

---
+++ Class_Assignments/Section5/exrand.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "coeffs.h"
+
+int main(void) // main function begins
+{
+
+    // random numbers
+    bernoulli("ber.dat", 1000000);
+    gen_Y("gen_Y.dat", 1000000, sqrt(10));
+
+    // Mean of uniform
+    // printf("%lf",mean("uni.dat"));
+    return 0;
+}
